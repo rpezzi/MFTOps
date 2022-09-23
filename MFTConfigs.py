9728bedb2fcd964d8d1ab70027dfe61685f12c8c
Rafael Pezzi <rafael.pezzi@cern.ch>
2022-09-23 12:06:05 +0200

    Restore 202 kHz Readout on PHYSICS configuration

--- MFTConfigs.py
+++ MFTConfigs.py
@@ -2,7 +2,7 @@
 configMap = {
 # Physics with beam and Cosmics 202.425 kHz (severe mask)
 "COSMICS": "./daq_init.py --dcs --gbtxload 3 --log --trig_source 4 --continuous -f 202.425 --auto_rof --scan 2 --mask 2 --name cru-",
-"PHYSICS": "./daq_init_gap12.py --dcs --gbtxload 3 --log --trig_source 4 --continuous -f 67.475 --auto_rof --scan 2 --mask 2 --name cru-",
+"PHYSICS": "./daq_init.py --dcs --gbtxload 3 --log --trig_source 4 --continuous -f 202.425 --auto_rof --scan 2 --mask 2 --name cru-",
 # Test 1:
 #"PHYSICS_GAP8_202": "./daq_init_gap8.py --gbtxload 3 --log --trig_source 4 --continuous -f 202.425 --auto_rof --scan 2 --mask 2 --name cru-",
 # Test 2:
