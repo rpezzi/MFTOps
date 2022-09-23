ea89db21d3a9bfc0d60a0e0a4bfdbce2eccc27e1
Rafael Pezzi <rafael.pezzi@cern.ch>
2022-09-23 12:09:20 +0200

    Comment out test configurations

--- MFTConfigs.py
+++ MFTConfigs.py
@@ -26,25 +26,25 @@ configMap = {
 # Test 10:
 #"PHYSICS_DCS": "./daq_init.py --dcs --gbtxload 3 --log --trig_source 4 --continuous -f 202.425 --auto_rof --scan 2 --mask 2 --name cru-",
 # Test 11:
-"PHYSICS_GAP8_202": "./daq_init_gap8.py --dcs --gbtxload 3 --log --trig_source 4 --continuous -f 202.425 --auto_rof --scan 2 --mask 2 --name cru-",
+#"PHYSICS_GAP8_202": "./daq_init_gap8.py --dcs --gbtxload 3 --log --trig_source 4 --continuous -f 202.425 --auto_rof --scan 2 --mask 2 --name cru-",
 # Test 12:
-"PHYSICS_GAP4_134": "./daq_init.py --dcs --gbtxload 3 --log --trig_source 4 --continuous -f 134.950 --auto_rof --scan 2 --mask 2 --name cru-",
+#"PHYSICS_GAP4_134": "./daq_init.py --dcs --gbtxload 3 --log --trig_source 4 --continuous -f 134.950 --auto_rof --scan 2 --mask 2 --name cru-",
 # Test 13:
-"PHYSICS_GAP8_134": "./daq_init_gap8.py --dcs --gbtxload 3 --log --trig_source 4 --continuous -f 134.950 --auto_rof --scan 2 --mask 2 --name cru-",
+#"PHYSICS_GAP8_134": "./daq_init_gap8.py --dcs --gbtxload 3 --log --trig_source 4 --continuous -f 134.950 --auto_rof --scan 2 --mask 2 --name cru-",
 # Test 14:
-"PHYSICS_GAP103_134": "./daq_init_gap103.py --dcs --gbtxload 3 --log --trig_source 4 --continuous -f 134.950 --auto_rof --scan 2 --mask 2 --name cru-",
+#"PHYSICS_GAP103_134": "./daq_init_gap103.py --dcs --gbtxload 3 --log --trig_source 4 --continuous -f 134.950 --auto_rof --scan 2 --mask 2 --name cru-",
 # Test 15:
-"PHYSICS_GAP20_202": "./daq_init_gap20.py --dcs --gbtxload 3 --log --trig_source 4 --continuous -f 202.425 --auto_rof --scan 2 --mask 2 --name cru-",
+#"PHYSICS_GAP20_202": "./daq_init_gap20.py --dcs --gbtxload 3 --log --trig_source 4 --continuous -f 202.425 --auto_rof --scan 2 --mask 2 --name cru-",
 # Test 15:
-"PHYSICS_GAP4_101": "./daq_init.py --dcs --gbtxload 3 --log --trig_source 4 --continuous -f 101.213 --auto_rof --scan 2 --mask 2 --name cru-",
+#"PHYSICS_GAP4_101": "./daq_init.py --dcs --gbtxload 3 --log --trig_source 4 --continuous -f 101.213 --auto_rof --scan 2 --mask 2 --name cru-",
 # Test 16:
-"PHYSICS_GAP12_101": "./daq_init_gap12.py --dcs --gbtxload 3 --log --trig_source 4 --continuous -f 101.213 --auto_rof --scan 2 --mask 2 --name cru-",
+#"PHYSICS_GAP12_101": "./daq_init_gap12.py --dcs --gbtxload 3 --log --trig_source 4 --continuous -f 101.213 --auto_rof --scan 2 --mask 2 --name cru-",
 # Test 17:
-"PHYSICS_GAP12_67": "./daq_init_gap12.py --dcs --gbtxload 3 --log --trig_source 4 --continuous -f 67.475 --auto_rof --scan 2 --mask 2 --name cru-",
+#"PHYSICS_GAP12_67": "./daq_init_gap12.py --dcs --gbtxload 3 --log --trig_source 4 --continuous -f 67.475 --auto_rof --scan 2 --mask 2 --name cru-",
 # Test 18:
-"PHYSICS_GAP8_67": "./daq_init_gap8.py --dcs --gbtxload 3 --log --trig_source 4 --continuous -f 67.475 --auto_rof --scan 2 --mask 2 --name cru-",
+#"PHYSICS_GAP8_67": "./daq_init_gap8.py --dcs --gbtxload 3 --log --trig_source 4 --continuous -f 67.475 --auto_rof --scan 2 --mask 2 --name cru-",
 # Test 19:
-"PHYSICS_GAP8_101": "./daq_init_gap8.py --dcs --gbtxload 3 --log --trig_source 4 --continuous -f 101.213 --auto_rof --scan 2 --mask 2 --name cru-",
+#"PHYSICS_GAP8_101": "./daq_init_gap8.py --dcs --gbtxload 3 --log --trig_source 4 --continuous -f 101.213 --auto_rof --scan 2 --mask 2 --name cru-",
 # Technical in Beam Tuning (RU ON, chips OFF) (Cosmics 202.425 kHz) TODO: ENSURE -tech can be in any position in the command line
 #"TECHNICAL": "./daq_init.py --gbtxload 3 --log --trig_source 4 --continuous -f 202.425 --auto_rof --scan 2 --mask 2 -tech --name cru-",
 # Technical in Beam Tuning (RU ON, chips OFF) (Cosmics 202.425 kHz) TODO: ENSURE -tech can be in any position in the command line
